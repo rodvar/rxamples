3715edffd2bed1c302b469f407cd717d4a4b27fa
Rodrigo Varela <rodvar@gmail.com>
2016-11-05 22:45:08 -0300

    Language level: Java8 , hello world example

--- app/src/main/java/com/rodvar/rxamples/MainActivity.java
+++ app/src/main/java/com/rodvar/rxamples/MainActivity.java
@@ -3,11 +3,26 @@ package com.rodvar.rxamples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rx.Observable;
+import rx.functions.Action1;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.rxJavaHelloWorld();
+        this.rxJavaMapExample();
+    }
+
+    private void rxJavaHelloWorld() {
+        Observable.just("Hello, world!")
+                .subscribe(s -> System.out.println(s));
+    }
+
+    private void rxJavaMapExample() {
+        
     }
 }
