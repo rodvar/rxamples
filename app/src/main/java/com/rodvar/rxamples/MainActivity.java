e6b2ba3bfd4af4c910067b97fc8583fe1ba12c0d
Rodrigo Varela <rodvar@gmail.com>
2016-11-05 23:49:28 -0300

    scan and window (this last one emits UnicastSubject objects

--- app/src/main/java/com/rodvar/rxamples/MainActivity.java
+++ app/src/main/java/com/rodvar/rxamples/MainActivity.java
@@ -3,6 +3,8 @@ package com.rodvar.rxamples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.functions.Action1;
 
@@ -14,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.rxJavaHelloWorld();
+
         this.rxJavaMapExample();
+        this.rxJavaScanExample();
+        this.rxJavaWindowExample();
     }
 
     private void rxJavaHelloWorld() {
@@ -22,7 +27,33 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe(s -> System.out.println(s));
     }
 
+    /**
+     * Main rx transformation function
+     */
     private void rxJavaMapExample() {
-        
+        Observable.just("Hello, world!")
+                .map(s -> s.replaceAll("o", " @ "))
+                .subscribe(s -> System.out.println(s));
+    }
+
+    /**
+     * Scans elements emited by the caller observable folding the previous result with the next item.
+     * First item is left as it is.
+     */
+    private void rxJavaScanExample() {
+        Observable.from(new Integer[]{0, 3, 5, 62, 4, 3, -77})
+                .scan( (i1,i2) -> i1 +i2)
+                .subscribe(i -> System.out.println(i));
+    }
+
+    /**
+     *
+     */
+    private void rxJavaWindowExample() {
+        Observable.from(new Integer[]{0, 3, 5, 62, 4, 3, -77})
+                .scan( (i1,i2) -> i1 +i2)
+                .window(1000l, TimeUnit.MILLISECONDS, 3)
+                .map(i -> String.format("%s emmited: %s", System.currentTimeMillis(), i.toString()))
+                .subscribe(s -> System.out.println(s));
     }
 }
